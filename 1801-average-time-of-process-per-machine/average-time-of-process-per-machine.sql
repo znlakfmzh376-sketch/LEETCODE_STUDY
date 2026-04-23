8db4f4595988df12058f55c8137e3f30295629f3
명품상자 <znlakfmzh376@gmail.com>
2026-04-23 16:29:01 +0900

    Time: 232 ms (78.68%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,6 +2,6 @@
 select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) as processing_time 
 from Activity a1
 join Activity a2
-on a1.machine_id = a2.machine_id
+on a1.machine_id = a2.machine_id and a1.process_id =a2.process_id 
 where (a1.activity_type='start') and (a2.activity_type='end')
 group by  machine_id;
