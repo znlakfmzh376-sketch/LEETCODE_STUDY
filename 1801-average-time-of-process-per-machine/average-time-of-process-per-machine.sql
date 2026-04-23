5e1e1be5fb08e5e05ec9d951e362925c8a7ff4af
명품상자 <znlakfmzh376@gmail.com>
2026-04-23 16:18:39 +0900

    Time: 233 ms (76.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) as processing_time 
+from Activity a1
+join Activity a2
+on a1.machine_id = a2.machine_id
+where (a1.activity_type='start') and (a2.activity_type='end')
+group by  machine_id;
